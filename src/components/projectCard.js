e4ebc65b1f392e80f008861a61948b2c3d862b6b
Nurmerey Shakhanova <shakhanova.n@gmail.com>
2019-10-28 14:51:43 -0700

    Add project card html content

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -7,9 +7,26 @@ const ProjectCard = () => {
       <div className="project-header">
         <p>Project Name here</p>
       </div>
+      <div>
+        <p>Lead: Rey Dekker - She's coding Seattle Chapter Lead</p>
+        <p>5 years experience</p>
+        <p>Currently: Facebook</p>
+      </div>
+
       <div className="project-body">
         <p className="project-description">This project aims to achieve...</p>
+        <div>
+          <p>Contributors</p>
+        </div>
+        <div>
+          <p>JS</p>
+          <p>ReactsJS</p>
+          <p>TypeScript</p>
+        </div>
       </div>
+      <button>Github Link</button>
+      <button>Favorite</button>
+      <button>Join</button>
     </div>
   );
 };
