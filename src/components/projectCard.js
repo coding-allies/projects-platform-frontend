9c073a768b64cfd21aa091f83007f73379b98bc7
Nurmerey Shakhanova <shakhanova.n@gmail.com>
2019-11-06 13:12:13 -0800

    Tags layout done

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,6 +1,39 @@
 import React from "react";
 import "../style/components/projectCard.css";
 
+const tags = [
+  "JS",
+  "ReactJS",
+  "Typescript",
+  "TypescriptTypescript",
+  "CSS",
+  "Objective C"
+];
+
+const getClassName = tag => {
+  switch (true) {
+    case tag.length < 6:
+      return "small";
+    case tag.length >= 6 && tag.length <= 19:
+      return "medium";
+    case tag.length > 19:
+      return "large";
+    default:
+      return "small";
+  }
+};
+
+const getTags = () => {
+  return tags.map(tag => {
+    const className = getClassName(tag);
+    return (
+      <div className={`tag ${className}`}>
+        <span>{tag}</span>
+      </div>
+    );
+  });
+};
+
 const ProjectCard = () => {
   return (
     <div className="card-wrapper">
@@ -22,11 +55,7 @@ const ProjectCard = () => {
         <div className="card-contributors">
           <p>Contributors</p>
         </div>
-        <div className="card-tags">
-          <div className="single-tag">JS</div>
-          <div className="single-tag">ReactsJS</div>
-          <div className="single-tag">TypeScript</div>
-        </div>
+        <div className="card-tags">{getTags()}</div>
       </div>
       <div className="card-buttons">
         <button>Github Link</button>
