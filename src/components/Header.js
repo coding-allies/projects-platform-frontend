7d27ba368b225411ae3df5e9b1dc3c809dada66a
Nevena Stoeva <nevena.stoeva@gmail.com>
2019-11-08 15:24:23 -0800

    Adds Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h1>she's collaborating</h1>
+        <button>Sign in with GitHub</button>
+      </header>
+    );
+  }
+}
+
+export default Header;
