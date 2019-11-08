6ebd19fd7e80bf88428326344fb9258a1f95dede
Nevena Stoeva <nevena.stoeva@gmail.com>
2019-11-08 15:24:23 -0800

    Adds Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
-import Projects from "./pages/projects";
+import Projects from "./pages/Projects";
+import Header from "./components/Header"
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Projects />
     </div>
   );
