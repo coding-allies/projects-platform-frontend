9c46e35fcab8164ccf84e49ba1b2edb023204560
Nevena Stoeva <nevena.stoeva@gmail.com>
2019-11-11 13:23:41 -0800

    Removes Platform Summary component from App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from "react";
 import logo from "./logo.svg";
 import Header from "./components/Header"
-import PlatformSummary from "./components/PlatformSummary"
 import Projects from "./pages/Projects";
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <PlatformSummary />
       <Projects />
     </div>
   );
