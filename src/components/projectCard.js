a85cace29432f55a0f8cdf8e4f5cf6ce7e1897ae
Nurmerey Shakhanova <shakhanova.n@gmail.com>
2019-11-11 11:57:42 -0800

    Setup data flow into the components

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -1,15 +1,6 @@
 import React from "react";
 import "../style/components/projectCard.css";
 
-const tags = [
-  "JS",
-  "ReactJS",
-  "Typescript",
-  "TypescriptTypescript",
-  "CSS",
-  "Objective C"
-];
-
 const getClassName = tag => {
   switch (true) {
     case tag.length < 6:
@@ -23,8 +14,8 @@ const getClassName = tag => {
   }
 };
 
-const getTags = () => {
-  return tags.map(tag => {
+const getTags = tagList => {
+  return tagList.map(tag => {
     const className = getClassName(tag);
     return (
       <div className={`tag ${className}`}>
@@ -34,37 +25,37 @@ const getTags = () => {
   });
 };
 
-const ProjectCard = () => {
+const getContributors = contributorList => {
+  return contributorList.map(contributor => (
+    <div className="card-contributor-icon">{contributor}</div>
+  ));
+};
+
+const ProjectCard = props => {
+  console.log("props", props.data);
+  const data = { ...props.data };
   return (
     <div className="card-wrapper">
       <header className="card-header">
-        <h2>Project Name here</h2>
+        <h2>{data.projectName}</h2>
       </header>
       <div className="card-lead">
-        <p>Lead: Rey Dekker - She's coding Seattle Chapter Lead</p>
-        <p>5 years experience, currently: Facebook</p>
+        <h4>{data.projectLead.name}</h4>
+        <p>
+          {data.projectLead.yearsOfExperience} years experience, currently:
+          {data.projectLead.currentPosition}
+        </p>
       </div>
 
       <article className="card-body">
-        <p className="card-description">
-          This project aims to achieve...Lorem ipsum dolor sit amet, consectetur
-          adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
-          magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-          ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        </p>
+        <p className="card-description">{data.description}</p>
         <div className="card-contributors">
-          <p className="card-contributor-label">Contributors - 3</p>
-          <div className="card-contributor-icon">NS</div>
-          <div className="card-contributor-icon">MM</div>
-          <div className="card-contributor-icon">NN</div>
-          <div className="card-contributor-icon">NS</div>
-          <div className="card-contributor-icon">MM</div>
-          <div className="card-contributor-icon">NN</div>
-          <div className="card-contributor-icon">NS</div>
-          <div className="card-contributor-icon">MM</div>
-          <div className="card-contributor-icon">NN</div>
+          <p className="card-contributor-label">
+            Contributors - {data.numberOfContributors}
+          </p>
+          {getContributors(data.contributors)}
         </div>
-        <div className="card-tags">{getTags()}</div>
+        <div className="card-tags">{getTags(data.tags)}</div>
       </article>
       <footer className="card-buttons">
         <button className="card-button">Github</button>
