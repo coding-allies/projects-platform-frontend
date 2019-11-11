1ab9137487085a0e153273d03b1fccb20bf45087
Nevena Stoeva <nevena.stoeva@gmail.com>
2019-11-11 14:10:13 -0800

    Adds Welcome page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
 import Header from "./components/Header"
 import PlatformSummary from "./components/PlatformSummary"
 import Projects from "./pages/Projects";
+import Welcome from "./pages/Welcome";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <PlatformSummary />
-      <Projects />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/">
+            <Welcome />
+          </Route>
+          <Route exact path="/projects">
+            <Projects />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
