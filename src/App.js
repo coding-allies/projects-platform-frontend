0ed5f46c3e972c6d9fbeca163700816948281c3b
Nevena Stoeva <1844100+partiallymisplaced@users.noreply.github.com>
2019-11-12 10:57:14 -0800

    Feature/welcome page (#11)
    * Adds react-router-dom
    
    * Adds Header component
    
    * Adds temporary styling to Header
    
    * Adds mock sign in/out functionality
    
    * Adds Platform Summary component
    
    * Adds Welcome page
    
    * Comments out Project Card components (to be fixed)
    
    * Adds missing Header
    
    * Removes version carets & npm
    
    * Removes reference to unused font (to be added later)
    
    * Removes reference to unused font (to be added later)
    
    * Adds TODO code comments
    
    * Adds media query for platform-features

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import logo from "./logo.svg";
-import Header from "./components/Header"
+import Header from "./components/Header";
 import Projects from "./pages/Projects";
+import Welcome from "./pages/Welcome";
 
 function App() {
+  /* TODO: Fix routing */
+  /* https://github.com/shescoding/projects-platform-frontend/issues/12 */
+  
   return (
     <div className="App">
       <Header />
-      <Projects />
+      <Router>
+        <Switch>
+          <Route path="/">
+            <Welcome />
+          </Route>
+          <Route path="/welcome">
+            <Projects />
+          </Route>
+        </Switch>
+      </Router>
+
     </div>
   );
 }
---
+++ src/pages/Welcome.js
@@ -0,0 +1,43 @@
+import React from "react";
+import ProjectCard from '../components/projectCard';
+import "../style/pages/welcome.css";
+
+function Welcome() {
+  /* TODO: Render ProjectCard components */
+  /* https://github.com/shescoding/projects-platform-frontend/issues/13 */
+
+  return (
+    <div className="platform-welcome">
+      <section className="platform-intro">
+          <h1>The Title Will Explain The Platform, Briefly</h1>
+          <p>The intro paragraph will go into a bit more details. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a mattis purus, vel cursus justo. Ut nec pretium ex. Nullam ac ultricies orci. Nulla facilisi.</p>
+          <button>Sign In With GitHub</button>
+      </section>
+      <section className="platform-features">
+        <div className="platform-feature">
+          <h3>Title Explains Feature</h3>
+          <p>The feature summary paragraph will go into a bit more details. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a mattis purus, vel cursus justo. Ut nec pretium ex.</p>
+        </div>
+        <div className="platform-feature">
+          <h3>Title Explains Feature</h3>
+          <p>The feature summary paragraph will go into a bit more details. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a mattis purus, vel cursus justo. Ut nec pretium ex.</p>
+        </div>
+        <div className="platform-feature">
+          <h3>Title Explains Feature</h3>
+          <p>The feature summary paragraph will go into a bit more details. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque a mattis purus, vel cursus justo. Ut nec pretium ex.</p>
+        </div>
+      </section>
+      <section className="platform-featured-projects">
+        <h2>Featured Projects</h2>
+        <div className="featured-projects-block">
+          {/* <ProjectCard /> */}
+          {/* <ProjectCard /> */}
+          {/* <ProjectCard /> */}
+        </div>
+      </section>
+      <footer>Brought to you by She's Coding</footer>
+    </div>
+  );
+}
+
+export default Welcome;
