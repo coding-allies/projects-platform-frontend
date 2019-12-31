c92cdba92a3e4a7ba16c5cce79307d2a9c1d5e6d
Nevena Stoeva <1844100+partiallymisplaced@users.noreply.github.com>
2019-12-31 11:38:05 -0800

    Feature/navigation (#36)
    * Updates Router setup (wraps Header, updates Route order)
    
    * Adds nav, converts Add Product button to link
    
    * Adds styles for button links
    
    * Adds styles for header nav
    
    * Updates styles to fix alignment issues
    
    * Incomplete: Adds redirect to Add project button (needs fixing)
    
    * Wraps Header in Router
    
    * Updates snapshot tests for App Header, AddProject
    
    * Removes Router from Header
    
    * Wraps Header in Memory Router (testing specific)

--- src/__tests__/components/Header.test.tsx
+++ src/__tests__/components/Header.test.tsx
@@ -1,9 +1,10 @@
 import * as React from "react";
+import {MemoryRouter as Router} from "react-router-dom";
 import renderer from "react-test-renderer";
 
 import Header from "../../components/Header";
 
 it("renders Header correctly", () => {
-  const component = renderer.create(<Header />).toJSON();
+  const component = renderer.create(<Router><Header /></Router>).toJSON();
   expect(component).toMatchSnapshot();
 });
