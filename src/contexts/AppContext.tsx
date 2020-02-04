3e8929909849c37a3e93a907c28f0b89e8cf724d
Rey Dekker <shakhanova.n@gmail.com>
2020-02-03 17:14:16 -0800

    Add project (#47)
    * First Try to connect to API
    
    * Receiving token auth
    
    * Added Token component to save it in a cookie
    
    * API success - Auth setup with cookie
    
    * Clean up
    
    * Sending add project data
    
    * Push to Projects page on add project success
    
    * Remove dismiss button
    
    * Indentation fix just for Nev

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -2,93 +2,36 @@ import * as React from "react";
 import { createContext, useState, useEffect } from "react";
 import { User, Project, AppContextState } from "../types";
 import axios from "axios";
+import { useCookies } from 'react-cookie';
+
 export const AppContext = createContext({});
 
 const mockUser: User = {
   name: "",
   is_authenticated: false,
+  csrf_token: "",
+  auth_token: "",
 };
 
 export const mockProjects: Array<Project> = [
-
-  // {
-  //   name: "Project Name here",
-  //   id: 18237,
-  //   lead: {
-  //     name: "Ada Lovelace",
-  //     experience: "Experienced (3+ years of experience)",
-  //     company: "Self-employed"
-  //   },
-  //   description:
-  //     "This project aims to achieve...Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  //   lookingFor:
-  //     "I could really use some help with enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  //   contributors: ["NS", "MM", "NN", "IJ", "FR", "HJ", "WS"],
-  //   tags: [
-  //     "JS",
-  //     "ReactJS",
-  //     "Typescript",
-  //     "TypescriptTypescript",
-  //     "CSS",
-  //     "Objective C"
-  //   ]
-  // },
-  // {
-  //   name: "Project 2 here context",
-  //   id: 34534,
-  //   lead: {
-  //     name: "Jane Doe",
-  //     experience: "Learner (0+ years of experience)",
-  //     company: "ABC Comp"
-  //   },
-  //   description:
-  //     "Some short description - Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  //   lookingFor: "I could really use some help with the front-end.",
-  //   contributors: ["JD"],
-  //   tags: ["JS", "ReactJS"]
-  // },
-  // {
-  //   name: "Project 2 here context",
-  //   id: 34535,
-  //   lead: {
-  //     name: "Sandy Doe",
-  //     experience: "Learner (0+ years of experience)",
-  //     company: "Test Company"
-  //   },
-  //   description:
-  //     "Some short description - Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  //   lookingFor:
-  //     "Any help with enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-  //   contributors: ["JD"],
-  //   tags: ["JS", "ReactJS"]
-  // },
-  // {
-  //   name: "Project 2 here context",
-  //   id: 34536,
-  //   lead: {
-  //     name: "Ginny Doe",
-  //     experience: "Learner (0+ years of experience)",
-  //     company: "ABC Comp"
-  //   },
-  //   description:
-  //     "Some short description - Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-  //   lookingFor: "We're just getting started, so any help is appreciated!",
-  //   contributors: ["JD"],
-  //   tags: ["JS", "ReactJS"]
-  // }
-
 ];
 
-export function AppContextProvider(props) {
+export const AppContextProvider = (props) => {
   const [user, setUser] = useState(mockUser);
   const [projects, setProjects] = useState(mockProjects);
 
+  const [cookies] = useCookies(['auth_token']);
+
+  const token = cookies.auth_token;
+  console.log("cookies in context", token);
   const fetchData = async () => {
-    axios.defaults.withCredentials = true;
+    // axios.defaults.withCredentials = true;
+
     const result = await axios(
-      "http://127.0.0.1:8000/projects/all/",
+      { method: 'get', url: "http://127.0.0.1:8000/projects/all/", headers: { Authorization: `Token ${token}` } }
     );
     console.log("xx", result.data.user);
+    result.data.user.auth_token = cookies.auth_token;
     setUser(result.data.user);
     setProjects(result.data.projects);
   };
--- src/pages/AddProject.tsx
+++ src/pages/AddProject.tsx
@@ -1,160 +1,190 @@
 import * as React from "react";
-import { Component } from "react";
-import { Redirect } from 'react-router-dom';
 import "../style/pages/AddProject.css";
+import axios from "axios";
+import { User } from "../types";
+import { AppContext } from "../contexts/AppContext";
+import { withRouter, RouteComponentProps } from 'react-router-dom';
+
 
 type FormState = {
   experienceLevel: string,
   currentLeadPosition: string,
   githubRepo: string,
-  lookingFor: string,  
+  lookingFor: string,
   techStack: string
 }
 
-class AddProject extends Component {
+class AddProject extends React.Component<RouteComponentProps, FormState> {
+  static contextType = AppContext;
+
   state: FormState = {
-    experienceLevel: "",
+    experienceLevel: "0",
     currentLeadPosition: "",
     githubRepo: "",
     lookingFor: "",
     techStack: ""
   }
 
+  csrf = '';
+
   handleChange = (event: (React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLTextAreaElement>)) => {
-    let name = event.target.name;
-    let value = event.target.value;
+    const name = event.target.name;
+    const value = event.target.value;
     this.setState({
       [name]: value
-    })
+    } as Pick<FormState, keyof FormState>)
+    console.log(event.target.name, "and ", event.target.value);
   }
 
-  // TODO: Redirect to projects page on submit (w/ history?)
-  handleSubmit(e: any) {
+  handleSubmit = (e: any) => {
     e.preventDefault();
-    console.log("it works");
-    return (
-      <Redirect to="/projects"/>
-    );
+    const stateDict = { ...this.state };
+    const user: User = this.context.user;
+    // TODO: tags
+
+    axios.post("http://127.0.0.1:8000/projects/add_project/", null, {
+      data: {
+        experience_lvl: stateDict.experienceLevel,
+        github_url: stateDict.githubRepo,
+        looking_for: stateDict.lookingFor,
+        position: stateDict.currentLeadPosition,
+        csrfmiddlewaretoken: this.csrf
+      },
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: `Token ${user.auth_token}`,
+      }
+    }
+    ).then((response) => {
+      console.log("Axios response", response);
+      this.props.history.push('/projects');
+    })
+      .catch(function (error) {
+        console.log(error);
+      });
+
+
   }
 
   render() {
     return (
       <div className="add-project-page">
-          <h1>Add your project</h1>
-          <div className="add-project-info-block">
-            <h2>Before we begin</h2>
-            <p>Make sure your GitHub profile is in order before submitting your project.</p>
-            <p>The following information will be visible:</p>
-            <ul>
-              <li>Name</li>
-              <li>Avatar</li>
-              <li>Project description</li>
-            </ul>
-            <button>Dismiss</button>
-          </div>
+        <h1>Add your project</h1>
+        <div className="add-project-info-block">
+          <h2>Before we begin</h2>
+          <p>Make sure your GitHub profile is in order before submitting your project.</p>
+          <p>The following information will be visible:</p>
+          <ul>
+            <li>Name</li>
+            <li>Avatar</li>
+            <li>Project description</li>
+          </ul>
+        </div>
 
         <form id="add-project-form" onSubmit={this.handleSubmit}>
-            <label
-              htmlFor="select-experience-level"
-              className="form-input-title">
-              Experience Level:
+          <label
+            htmlFor="select-experience-level"
+            className="form-input-title">
+            Experience Level:
             </label>
-            <label
-              htmlFor="select-experience-level"
-              className="form-input-description">
-              Select the description that best matches your experience
+          <label
+            htmlFor="select-experience-level"
+            className="form-input-description">
+            Select the description that best matches your experience
               </label>
-            {/* TODO: Experience level from types */}
-            <select
-              id="select-experience-level"
-              name="experienceLevel"
-              value={this.state.experienceLevel}
-              onChange={this.handleChange}>
-              <option value="">Select your experience level</option>
-              <option value="learner">Learner (0+ years of experience)</option>
-              <option value="beginner">Beginner (1+ years of experience)</option>
-              <option value="experienced">Experienced (3+ years of experience)</option>
-            </select>
+          {/* TODO: Experience level from types */}
+          <select
+            id="select-experience-level"
+            name="experienceLevel"
+            value={this.state.experienceLevel}
+            onChange={this.handleChange}>
+            {/* <option value="">Select your experience level</option> */}
+            <option value={0}>Learner (0+ years of experience)</option>
+            <option value={1}>Beginner (1+ years of experience)</option>
+            <option value={2}>Experienced (3+ years of experience)</option>
+          </select>
 
-            <label
-              htmlFor="lead-position"
-              className="form-input-title">
-              Current Position:
+          <label
+            htmlFor="lead-position"
+            className="form-input-title">
+            Current Position:
             </label>
-            <label
-              htmlFor="lead-position"
-              className="form-input-description">
-              Enter your current position (i.e. "Engineer at Facebook", "Bootcamp Grad from Galvanize")
+          <label
+            htmlFor="lead-position"
+            className="form-input-description">
+            Enter your current position (i.e. "Engineer at Facebook", "Bootcamp Grad from Galvanize")
             </label>
-            <input
-              type="text"
-              id="lead-position"
-              name="currentLeadPosition"
-              onChange={this.handleChange}
-              value={this.state.currentLeadPosition}
-            />
+          <input
+            type="text"
+            id="lead-position"
+            name="currentLeadPosition"
+            onChange={this.handleChange}
+            value={this.state.currentLeadPosition}
+          />
 
-            <label
-              htmlFor="github-repo-input"
-              className="form-input-title">
-              GitHub Repo:
+          <label
+            htmlFor="github-repo-input"
+            className="form-input-title">
+            GitHub Repo:
             </label>
-            <label
-              htmlFor="github-repo-input"
-              className="form-input-description">
-              Add a link to the GitHub repository for your project
+          <label
+            htmlFor="github-repo-input"
+            className="form-input-description">
+            Add a link to the GitHub repository for your project
             </label>
-            <input
-              type="url"
-              id="github-repo-input"
-              name="githubRepo"
-              value={this.state.githubRepo}
-              onChange={this.handleChange}
-            />
+          <input
+            type="url"
+            id="github-repo-input"
+            name="githubRepo"
+            value={this.state.githubRepo}
+            onChange={this.handleChange}
+          />
 
-            <label
-              htmlFor="looking-for"
-              className="form-input-title">
-              Looking for:
+          <label
+            htmlFor="looking-for"
+            className="form-input-title">
+            Looking for:
             </label>
-            <label
-              htmlFor="looking-for"
-              className="form-input-description">
-              Describe what are you looking for in future contributors
+          <label
+            htmlFor="looking-for"
+            className="form-input-description">
+            Describe what are you looking for in future contributors
             </label>
-            {/* TODO: Add fixed values for cols, rows */}
-            <textarea
-              id="looking-for"
-              name="lookingFor"
-              value={this.state.lookingFor}
-              onChange={this.handleChange}
-              style={{"resize": "none"}}
-            />
+          {/* TODO: Add fixed values for cols, rows */}
+          <textarea
+            id="looking-for"
+            name="lookingFor"
+            value={this.state.lookingFor}
+            onChange={this.handleChange}
+            style={{ "resize": "none" }}
+          />
 
-            <label
-              htmlFor="tech-stack"
-              className="form-input-title">
-              Tech Stack:
+          <label
+            htmlFor="tech-stack"
+            className="form-input-title">
+            Tech Stack:
             </label>
-            <label
-              htmlFor="tech-stack"
-              className="form-input-description">
-              Enter the tech stack your project is built with, separated by commas. Max 5.
+          <label
+            htmlFor="tech-stack"
+            className="form-input-description">
+            Enter the tech stack your project is built with, separated by commas. Max 5.
             </label>
-            <input
-              type="text"
-              id="tech-stack"
-              name="techStack"
-              value={this.state.techStack}
-              onChange={this.handleChange}
-            />
-            <button>
-              Add your project
-            </button>
-          </form>
+          <input
+            type="text"
+            id="tech-stack"
+            name="techStack"
+            value={this.state.techStack}
+            onChange={this.handleChange}
+          />
+          <button>
+            Add your project
+          </button>
+        </form>
       </div>
     );
   }
 }
 
-export default AddProject;
+export default withRouter(AddProject);
