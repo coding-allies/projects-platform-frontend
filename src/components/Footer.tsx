191cc965df417ab3242f8fe4180ba1279be9f520
Nev <nevena.stoeva@gmail.com>
2020-02-06 13:02:36 -0800

    Adds Footer component and includes on App level

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+
+const Footer = () => {
+    return (
+      <footer>
+        Brought to you with love from <a href="http://shescoding.org/" target="_blank" rel="noopener noreferrer">
+          She's Coding
+        </a>
+      </footer>
+    );
+}
+
+export default Footer;
