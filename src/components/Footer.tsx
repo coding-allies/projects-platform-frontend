ec4001616ca304916299efcb8bc95810d8e4def6
Nevena Stoeva <1844100+partiallymisplaced@users.noreply.github.com>
2020-02-06 15:25:00 -0800

    Add Copy, Footer (#52)
    * Moves welcome to Platform Summary
    
    * Adds edited copy to Platform Summary
    
    * Removes footer from Welcome page
    
    * Adds Footer component and includes on App level
    
    * Adds style to Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import "../style/components/Footer.css"
+
+const Footer = () => {
+    return (
+      <footer>
+        Brought to you with love by <a href="http://shescoding.org/" target="_blank" rel="noopener noreferrer">
+          She's Coding
+        </a>
+      </footer>
+    );
+}
+
+export default Footer;
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import { Component } from "react";
 import { AppContext } from "../contexts/AppContext";
 import { Project } from "../types";
+import PlatformSummary from "../components/PlatformSummary";
 import { renderProjects } from "./Projects"
 import "../style/pages/Welcome.css";
 import "../style/pages/Projects.css";
@@ -12,50 +13,12 @@ class Welcome extends Component {
     const projects: Array<Project> = this.context.projects;
     return (
       <div className="platform-welcome">
-        <section className="platform-intro">
-          <h1>The Title Will Explain The Platform, Briefly</h1>
-          <p>
-            The intro paragraph will go into a bit more details. Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit. Quisque a mattis purus, vel
-            cursus justo. Ut nec pretium ex. Nullam ac ultricies orci. Nulla
-            facilisi.
-          </p>
-          <button>Sign In With GitHub</button>
-        </section>
-        <section className="platform-features">
-          <div className="platform-feature">
-            <h3>Title Explains Feature</h3>
-            <p>
-              The feature summary paragraph will go into a bit more details. Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit. Quisque a mattis
-              purus, vel cursus justo. Ut nec pretium ex.
-            </p>
-          </div>
-          <div className="platform-feature">
-            <h3>Title Explains Feature</h3>
-            <p>
-              The feature summary paragraph will go into a bit more details. Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit. Quisque a mattis
-              purus, vel cursus justo. Ut nec pretium ex.
-            </p>
-          </div>
-          <div className="platform-feature">
-            <h3>Title Explains Feature</h3>
-            <p>
-              The feature summary paragraph will go into a bit more details. Lorem
-              ipsum dolor sit amet, consectetur adipiscing elit. Quisque a mattis
-              purus, vel cursus justo. Ut nec pretium ex.
-            </p>
-          </div>
-        </section>
+        <PlatformSummary />
         <section className="platform-featured-projects">
           <h2>Featured Projects</h2>
           <div className="projects-wrapper">{renderProjects(projects)}</div>
         </section>
-        <footer>Brought to you by She's Coding</footer>
       </div>
-
-
     );
   }
 }
