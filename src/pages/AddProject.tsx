0505517e038d863fac57c21b0bb892d1fe8dc2bd
Nevena Stoeva <1844100+partiallymisplaced@users.noreply.github.com>
2020-02-10 10:33:40 -0800

    Adds front-end validation (#53)
    * Adds preliminary validation
    
    * Adds classnames for conditional styling
    
    * Updates Regex match for GitHub URL
    
    * Adds classnames to input fields
    
    * Styles errors
    
    * Triggers validation on blur
    
    * Experience levels with errors, return strings
    
    * Adds validation for fields, errors
    
    * Convert Github url to API url
    
    * Prototype for validation on submit
    
    * Completes ShowErrors on submit
    
    * Adds a general error for server validation, styles
    
    * Set server error
    
    * Clean up
    
    Co-authored-by: Rey Dekker <shakhanova.n@gmail.com>

--- src/pages/AddProject.tsx
+++ src/pages/AddProject.tsx
@@ -1,68 +1,200 @@
 import * as React from "react";
 import "../style/pages/AddProject.css";
+import classNames from "classnames/bind"
+import axios from "axios";
 import { AppContext } from "../contexts/AppContext";
 import { withRouter, RouteComponentProps } from 'react-router-dom';
 
+const validGitHubRepo = /^(?:https:\/\/)*github[.]com\/([a-z0-9-]+)\/([a-z0-9\-_]+)\/?$/;
 
 type FormState = {
   experienceLevel: string,
   currentLeadPosition: string,
   githubRepo: string,
   lookingFor: string,
-  techStack: string
+  // techStack: string,
+  errors: {
+    experienceLevel: string,
+    currentLeadPosition: string,
+    githubRepo: string,
+    lookingFor: string,
+    // techStack: string,
+  },
+  serverError: {
+    message: string
+  }
+}
+
+const validateForm = (state) => {
+  let noErrors = true;
+  let hasInput = false;
+
+  let inputFields = Object.values(state).filter(
+    (value: any) => value.length === 0);
+
+  hasInput = (inputFields.length === 0) ? true : false;
+
+  Object.values(state.errors).forEach(
+    (value: any) => value.length > 0 && (noErrors = false)
+  );
+
+  return hasInput && noErrors;
 }
 
 class AddProject extends React.Component<RouteComponentProps, FormState> {
   static contextType = AppContext;
 
   state: FormState = {
-    experienceLevel: "0",
+    experienceLevel: "",
     currentLeadPosition: "",
     githubRepo: "",
     lookingFor: "",
-    techStack: ""
+    // techStack: "",
+    errors: {
+      experienceLevel: "",
+      currentLeadPosition: "",
+      githubRepo: "",
+      lookingFor: "",
+      // techStack: "",
+    },
+    serverError: {
+      message: ""
+    }
   }
 
   csrf = '';
 
-  handleChange = (event: (React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLSelectElement> | React.ChangeEvent<HTMLTextAreaElement>)) => {
+  isPublicGithubRepo = (githubUrl) => {
+    const gitURLSplit = githubUrl.split("github.com/");
+    const gitAPIURL = "https://api.github.com/repos/" + gitURLSplit[gitURLSplit.length - 1];
+    axios.get(gitAPIURL)
+      .then((response) => console.log(response.status))
+      .catch((error) => console.log(error));
+    return true;
+  }
+
+  handleChange = (event: any) => {
     const name = event.target.name;
     const value = event.target.value;
+    const errors = this.state.errors;
+
+    switch (name) {
+      case 'experienceLevel':
+        errors.experienceLevel =
+          value.length === 0 ? "Experience level is required" : "";
+        break;
+
+      case 'currentLeadPosition':
+        errors.currentLeadPosition =
+          value.length === 0 ? "Current position is required" : "";
+        break;
+
+      case 'githubRepo':
+        if (value.length === 0) {
+          errors.githubRepo = "A GitHub repository is required"
+        }
+        if (!validGitHubRepo.test(value)) {
+          errors.githubRepo = "Please enter a valid GitHub repository";
+        } else {
+          errors.githubRepo = "";
+          if (!this.isPublicGithubRepo(value)) {
+            errors.githubRepo = "This GitHub repository could not be found";
+            // if (!this.isNotListed(value)) {
+            //   errors.githubRepo = "This repository is already listed under another lead";
+            // }
+          }
+        }
+        break;
+
+      case 'lookingFor':
+        errors.lookingFor =
+          value.length === 0 ? "Field cannot be empty" : "";
+        break;
+
+      // case 'techStack':
+      //   errors.techStack =
+      //     value.length === 0 ? "Tech stack cannot be empty" : "";
+      //   break;
+
+      default:
+        break;
+
+    }
+
     this.setState({
-      [name]: value
+      [name]: value,
+      errors
     } as Pick<FormState, keyof FormState>)
   }
 
-  handleSubmit = (e: any) => {
-    e.preventDefault();
-    const stateDict = { ...this.state };
-    // TODO: tags
-    const data = {
-      experience_lvl: stateDict.experienceLevel,
-      github_url: stateDict.githubRepo,
-      looking_for: stateDict.lookingFor,
-      position: stateDict.currentLeadPosition,
-      csrfmiddlewaretoken: this.csrf
+  showErrors = () => {
+    let currErrors = this.state.errors;
+    if (!!!this.state.experienceLevel) {
+      currErrors.experienceLevel = "Experience level is required";
     }
-    this.context.addProject(data).then((response) => {
-      console.log("response xx", response);
-      if (response.data["result"] === "success") {
-        this.props.history.push('/projects');
-      } else if (response.data["result"] === "error") {
-        // TODO: display the message to the users
-        console.log("response", response.data["message"]);
-      } else {
-        console.log("response", response);
-      }
-    })
-      .catch(function (error) {
-        console.log(error);
-      });
 
+    if (!!!this.state.currentLeadPosition) {
+      currErrors.currentLeadPosition = "Current position is required";
+    }
+
+    if (!!!this.state.githubRepo) {
+      currErrors.githubRepo = "A GitHub repository is required";
+    }
+
+    if (!!!this.state.githubRepo) {
+      currErrors.githubRepo = "A GitHub repository is required";
+    }
+
+    if (!!!this.state.lookingFor) {
+      currErrors.lookingFor = "Field cannot be empty"
+    }
+
+    // if (!!!this.state.techStack) {
+    //   currErrors.techStack = "Tech stack cannot be empty"
+    // }
+
+    this.setState({ errors: currErrors });
+  }
+
+  handleSubmit = (event: any) => {
+    // let that = this;
+    event.preventDefault();
+    if (validateForm(this.state)) {
+
+      console.log("Valid form");
+
+      const stateDict = { ...this.state };
+      // TODO: tags
+      const data = {
+        experience_lvl: stateDict.experienceLevel,
+        github_url: stateDict.githubRepo,
+        looking_for: stateDict.lookingFor,
+        position: stateDict.currentLeadPosition,
+        csrfmiddlewaretoken: this.csrf
+      }
+      this.context.addProject(data).then((response) => {
+        if (response.data["result"] === "success") {
+          this.props.history.push('/projects');
+        } else if (response.data["result"] === "error") {
+          let currServerError = this.state.serverError;
+          currServerError.message = response.data["message"];
+          this.setState({ serverError: currServerError });
+        } else {
+          console.log("response", response);
+        }
+      })
+        .catch(error => {
+          console.log(error);
+        });
+    } else {
+      console.log("Invalid form");
+      this.showErrors();
+    }
 
   }
 
   render() {
+    const { errors } = this.state;
     return (
       <div className="add-project-page">
         <h1>Add your project</h1>
@@ -77,101 +209,120 @@ class AddProject extends React.Component<RouteComponentProps, FormState> {
           </ul>
         </div>
 
-        <form id="add-project-form" onSubmit={this.handleSubmit}>
+        {(this.state.serverError.message) && <div className="error-message">
+          <p>{this.state.serverError.message}</p>
+          <p>If this problem persists, contact us!</p>
+        </div>}
+        <form id="add-project-form" onSubmit={this.handleSubmit} noValidate>
           <label
             htmlFor="select-experience-level"
             className="form-input-title">
             Experience Level:
-            </label>
+          </label>
           <label
             htmlFor="select-experience-level"
             className="form-input-description">
             Select the description that best matches your experience
-              </label>
-          {/* TODO: Experience level from types */}
+          </label>
           <select
             id="select-experience-level"
             name="experienceLevel"
             value={this.state.experienceLevel}
-            onChange={this.handleChange}>
-            {/* <option value="">Select your experience level</option> */}
-            <option value={0}>Learner (0+ years of experience)</option>
-            <option value={1}>Beginner (1+ years of experience)</option>
-            <option value={2}>Experienced (3+ years of experience)</option>
+            onChange={this.handleChange}
+            onBlur={this.handleChange}
+            className={classNames({ "field-error": errors.experienceLevel.length > 0 })}>
+            <option value="">Select your experience level</option>
+            <option value="0">Learner (0+ years of experience)</option>
+            <option value="1">Beginner (1+ years of experience)</option>
+            <option value="2">Experienced (3+ years of experience)</option>
           </select>
+          {errors.experienceLevel.length > 0 && <span className="error">{errors.experienceLevel}</span>}
 
           <label
             htmlFor="lead-position"
             className="form-input-title">
             Current Position:
-            </label>
+          </label>
           <label
             htmlFor="lead-position"
             className="form-input-description">
             Enter your current position (i.e. "Engineer at Facebook", "Bootcamp Grad from Galvanize")
-            </label>
+          </label>
           <input
             type="text"
             id="lead-position"
             name="currentLeadPosition"
             onChange={this.handleChange}
+            onBlur={this.handleChange}
             value={this.state.currentLeadPosition}
+            className={classNames({ "field-error": errors.currentLeadPosition.length > 0 })}
           />
+          {errors.currentLeadPosition.length > 0 && <span className="error">{errors.currentLeadPosition}</span>}
 
           <label
             htmlFor="github-repo-input"
             className="form-input-title">
             GitHub Repo:
-            </label>
+          </label>
           <label
             htmlFor="github-repo-input"
             className="form-input-description">
             Add a link to the GitHub repository for your project
-            </label>
+          </label>
           <input
             type="url"
             id="github-repo-input"
             name="githubRepo"
             value={this.state.githubRepo}
             onChange={this.handleChange}
+            onBlur={this.handleChange}
+            className={classNames({ "field-error": errors.githubRepo.length > 0 })}
           />
+          {errors.githubRepo.length > 0 && <span className="error">{errors.githubRepo}</span>}
 
           <label
             htmlFor="looking-for"
             className="form-input-title">
             Looking for:
-            </label>
+          </label>
           <label
             htmlFor="looking-for"
             className="form-input-description">
             Describe what are you looking for in future contributors
-            </label>
+          </label>
           {/* TODO: Add fixed values for cols, rows */}
           <textarea
             id="looking-for"
             name="lookingFor"
             value={this.state.lookingFor}
             onChange={this.handleChange}
+            onBlur={this.handleChange}
             style={{ "resize": "none" }}
+            className={classNames({ "field-error": errors.lookingFor.length > 0 })}
           />
+          {errors.lookingFor.length > 0 && <span className="error">{errors.lookingFor}</span>}
 
           {/* <label
             htmlFor="tech-stack"
             className="form-input-title">
             Tech Stack:
-            </label>
+          </label>
           <label
             htmlFor="tech-stack"
             className="form-input-description">
             Enter the tech stack your project is built with, separated by commas. Max 5.
-            </label>
+          </label>
           <input
             type="text"
             id="tech-stack"
             name="techStack"
             value={this.state.techStack}
             onChange={this.handleChange}
-          /> */}
+            onBlur={this.handleChange}
+            className={classNames({ "field-error": errors.techStack.length > 0 })}
+          />
+          {errors.techStack.length > 0 && <span className="error">{errors.techStack}</span>}
+          */}
           <button>
             Add your project
           </button>
