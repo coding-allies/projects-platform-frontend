267061e728d1ff68145ba580f4b27986320df589
leyla li <leylali@qq.com>
2020-04-29 15:32:35 -0700

    add env file and setup client db

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+const pg = require('pg');
+
 
 require('dotnev').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const client = new pg.Client(process.env.DATABASE_URL);
+client.connect();
+client.on('error', (err)=> console.log(err));
 
 app.use(cors());
 app.use(express.json());
