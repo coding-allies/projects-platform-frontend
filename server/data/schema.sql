6c3a809c157cbe3007781ad94fad914919bccf26
leyla li <leylali@qq.com>
2020-05-03 09:55:45 -0700

    connect db to server and create projects and users tables

---
+++ server/data/schema.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Projects;
+
+CREATE TABLE Projects(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  github_url VARCHAR(255),
+  description TEXT,
+  looking_for TEXT,
+  created TIMESTAMPTZ,
+  updated TIMESTAMPTZ,
+  lead_id INTEGER,
+  contributors TEXT,
+  tags TEXT
+);
+
+CREATE TABLE Users(
+  id SERIAL PRIMARY KEY,
+  experience_lvl SMALLINT,
+  position VARCHAR(255),
+  github_username VARCHAR(255),
+  github_id INTEGER,
+  github_url VARCHAR(255),
+  avatar_url VARCHAR(255),
+  gravatar_url VARCHAR(255),
+  last_login TIMESTAMP,
+  is_superuser BOOLEAN,
+  username VARCHAR(150),
+  first_name VARCHAR(30),
+  last_name VARCHAR(150),
+  email VARCHAR(254),
+  is_active BOOLEAN,
+  date_joined TIMESTAMPTZ
+);
