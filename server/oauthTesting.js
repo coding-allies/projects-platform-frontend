6aca8da1246ed95d3f69699e59943dfb508821c4
leyla li <leylali@qq.com>
2020-06-02 19:26:27 -0700

    test it with axios instead of fetch

--- server/oauthTesting.js
+++ server/oauthTesting.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const fetch = require('node-fetch');
+// const fetch = require('node-fetch');
 // const cookieSession = require('cookie-session');
+const axios = require('axios');
 
 const app = express();
 
@@ -9,7 +10,7 @@ require('dotenv').config();
 
 const client_id = process.env.GITHUB_KEY;
 const client_secret = process.env.GITHUB_SECRET;
-console.log({ client_id, client_secret });
+// console.log({ client_id, client_secret });
 
 app.get("/", (req, res) => {
   res.send("Hello GitHub auth");
@@ -18,26 +19,52 @@ app.get("/", (req, res) => {
 app.get("/auth/github", (req, res) => {
   const redirect_uri = "http://localhost:5000/auth/github/callback";
   res.redirect(
-    `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}`
+    `https://github.com/login/oauth/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&scope=user`
   );
 });
 
 async function getAccessToken(code, client_id, client_secret) {
-  const res = await fetch("https://github.com/login/oauth/access_token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: {
-      "client_id":client_id,
-      "client_secret":client_secret,
-      "code":code
+  console.log('this is the arguments for post request',{ client_id, client_secret, code });
+
+  // const res = await fetch("https://github.com/login/oauth/access_token", {
+  //   method: "POST",
+  //   headers: {
+  //     "content-type": "application/json"
+  //   },
+  //   body: {
+  //     "client_id":client_id,
+  //     "client_secret":client_secret,
+  //     "code":code
+  //   }
+  // });
+
+  const result = await axios(
+    {
+      method: 'post', 
+      url: "https://github.com/login/oauth/access_token",
+      data: {
+            client_id:client_id,
+            client_secret:client_secret,
+            code:code
+          } 
     }
-  });
-  const text = await res.text();
-  console.log(text);
-  const params = new URLSearchParams(text);
-  return params.get("access_token");
+  )
+    .then(data => {console.log('Success ' + JSON.stringify(data))})
+    .catch(err => {
+      console.log('Error ' + err.message)
+    })
+
+  console.log(result);
+
+
+  // console.log('this is the headers', res.headers);
+
+  // const text = await res.text();
+  // console.log('this is the res from callback', res);
+  // // console.log('this is the headers', res.headers);
+  // // console.log('this is res to text',text);
+  // const params = new URLSearchParams(text);
+  // return params.get("access_token");
 }
 
 // async function fetchGitHubUser(token) {
