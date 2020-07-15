686940ac0a805686abe34dc158559560169e0a6c
Nurmerey Shakhanova <shakhanova.n@gmail.com>
2020-07-14 19:48:32 -0700

    Clean up

--- server/github_oauth.js
+++ server/github_oauth.js
@@ -2,25 +2,18 @@
 const { URLSearchParams } = require('url');
 const express = require('express');
 const router = express.Router();
-
 const pg = require('pg');
 const jwt = require('jsonwebtoken');
 const fetch = require('node-fetch');
 const axios = require('axios');
 
-
 require('dotenv').config();
-
 const client_id = process.env.GITHUB_KEY;
 const client_secret = process.env.GITHUB_SECRET;
 
-
 //connect to the database
 const client = new pg.Client(process.env.DATABASE_URL);
-console.log('xxx', process.env.DATABASE_URL);
-client.connect();
-client.on('error', (err) => console.log(err));
-
+client.connect()
 
 function User(data) {
   this.token = data.token ? data.token : '';
@@ -41,14 +34,12 @@ function User(data) {
   this.date_joined = data.date_joined ? data.date_joined : null;
 }
 
-
 router.get("/", (req, res) => {
   res.send("Hello GitHub auth");
 });
 
 router.get("/auth/github", (req, res) => {
   const url = `https://github.com/login/oauth/authorize?client_id=${client_id}&scope=user:email`;
-  console.log('XXX url', url);
   res.redirect(url);
 });
 
@@ -56,10 +47,8 @@ router.get("/auth/github/callback", async (req, res) => {
   const code = req.query.code;
   let access_token = await getAccessToken(code, client_id, client_secret)
     .then((access_token) => {
-      console.log("CALLBACK access_token", access_token);
       return access_token;
     }).catch(error => {
-      console.error("Error", error);
       return null;
     });
   if (access_token === null) {
@@ -67,9 +56,7 @@ router.get("/auth/github/callback", async (req, res) => {
   }
   await fetchGitHubUser(access_token)
     .then(async (user) => {
-      console.log('this is user within user', user);
       if (user) {
-        console.log('this is the req from call back');
         const user_token = jwt.sign(
           { userId: user.id },
           process.env.TOKEN_SECRET,
@@ -80,16 +67,11 @@ router.get("/auth/github/callback", async (req, res) => {
       }
     })
     .catch(err => {
-      console.log(`This is an error from fetching github user ${err}`);
       res.status(400).send("Error: Could not get user from Github");
     });
-
 });
 
-
 async function getAccessToken(code, client_id, client_secret) {
-  console.log('this is the arguments for post request', code, client_id, client_secret);
-
   const result = await axios(
     {
       method: 'post',
@@ -101,19 +83,15 @@ async function getAccessToken(code, client_id, client_secret) {
       }
     }
   ).then(data => {
-    console.log('Success ', data.data);
     const params = new URLSearchParams(data.data);
-    console.log("XXX access token", params.get("access_token"));
     const access_token = params.get("access_token");
     return access_token;
   })
     .catch(err => {
-      console.log('Error ' + err.message);
       throw err;
     });
   return result;
 }
-
 
 async function fetchGitHubUser(token) {
   const request = await fetch("https://api.github.com/user", {
@@ -121,45 +99,34 @@ async function fetchGitHubUser(token) {
       Authorization: "token " + token
     }
   });
-
   return await request.json();
 }
 
-
-
 async function checkUser(user_data, token, res) {
   let SQL = 'SELECT * FROM Users WHERE github_id=$1;';
-  console.log('this is the userid from checkUser', user_data.id);
   let values = [user_data.id];
   await client.query(SQL, values)
     .then(result => {
       const user = result.rows[0];
-      console.log('here is the user from SQL', user);
       if (user !== undefined) {
-        console.log('xxxx have user within authentication', user.token);
         let SQL = 'UPDATE Users SET token = $1 WHERE token=$2;';
         let values = [token, user.token];
         client.query(SQL, values)
           .then(result => {
-            console.log('here is the user with new token', result);
             res.redirect("/");
           })
           .catch(err => {
-            console.log(err);
             throw err;
           });
       } else {
-        console.log('xxx get in creating new user');
         createUser(user_data, token);
         res.redirect("/");
       }
     })
     .catch(err => {
-      console.log(err);
       throw err;
     });
 }
-
 
 function createUser(user_data, user_token) {
   const newUser = new User({
@@ -170,19 +137,12 @@ function createUser(user_data, user_token) {
     avatar_url: user_data.avatar_url,
     gravatar_url: user_data.gravatar_id
   });
-  console.log('XXXX this is the new user created', newUser);
 
   // save user to sql
   let SQL = 'INSERT INTO users (token, experience_lvl, position, github_username, github_id, github_url, avatar_url, gravatar_url, last_login, is_superuser, username, first_name, last_name, email, is_active, date_joined) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16) RETURNING id;';
   let values = [newUser.token, newUser.experience_lvl, newUser.position, newUser.github_username, newUser.github_id, newUser.github_url, newUser.avatar_url, newUser.gravatar_url, newUser.last_login, newUser.is_superuser, newUser.username, newUser.first_name, newUser.last_name, newUser.email, newUser.is_active, newUser.date_joined];
-
-  // console.log("the query", SQL);
   client.query(SQL, values)
-    .then(result => console.log('XXXX got in sql saving', result))
-    .catch(err => { console.log(err); throw err; });
-
 }
-
 
 // router.get("/logout", (req, res) => {
 //   if (req.session) req.session = null;
